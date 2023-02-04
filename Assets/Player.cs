5f6da50f4d72ec58e638bda714ca977758a89603
Xavier Clavel <xrclavel@gmail.com>
2023-02-04 08:13:01 +0100

    root placing

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,14 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
-enum state { placing, controlling };
+enum state { placing, controlling, animating };
 
 public class Player : MonoBehaviour
 {
     [SerializeField] Tilemap tilemap;
     [SerializeField] GameObject placeArrow;
     [SerializeField] Vector2 mapSize;
+    [SerializeField] List<TileBase> obstacles;
+    [SerializeField] TileBase rootTile;
     state gameState = state.placing;
+    Vector3Int currentPosition;
     Controls controls;
 
     private void Start()
@@ -20,6 +23,7 @@ public class Player : MonoBehaviour
         controls.Game.MoveRight.performed += ctx => MoveRight();
         controls.Game.MoveLeft.performed += ctx => MoveLeft();
         controls.Game.MoveDown.performed += ctx => MoveDown();
+        controls.Game.MoveUp.performed += ctx => MoveUp();
 
         int minPos = tilemap.cellBounds.xMin;
         int maxPos = tilemap.cellBounds.xMax;
@@ -44,19 +48,77 @@ public class Player : MonoBehaviour
 
     void MoveRight()
     {
-        if (placeArrow.transform.position.x >= mapSize.x * 0.5f) return;
-        placeArrow.transform.position += Vector3.right;
+        if (gameState == state.placing)
+        {
+            if (placeArrow.transform.position.x >= mapSize.x * 0.5f) return;
+            placeArrow.transform.position += Vector3.right;
+        }
+        else if (gameState == state.controlling)
+        {
+            TryFillDirection(Vector3Int.right);
+        }
+
     }
 
     void MoveLeft()
     {
-        if (placeArrow.transform.position.x <= -mapSize.x * 0.5f) return;
-        placeArrow.transform.position += Vector3.left;
+        if (gameState == state.placing)
+        {
+            if (placeArrow.transform.position.x <= -mapSize.x * 0.5f) return;
+            placeArrow.transform.position += Vector3.left;
+        }
+        else if (gameState == state.controlling)
+        {
+            TryFillDirection(Vector3Int.left);
+        }
     }
 
     void MoveDown()
     {
-        RaycastHit2D hit = Physics2D.Raycast(placeArrow.transform.position, 99 * Vector2.down);
-        Debug.Log(hit.transform.position);
+        currentPosition = tilemap.WorldToCell(placeArrow.transform.position);
+        if (gameState == state.placing)
+        {
+            TryFillDirection(Vector3Int.down);
+            placeArrow.SetActive(false);
+        }
+        else if (gameState == state.controlling)
+        {
+            TryFillDirection(Vector3Int.down);
+        }
+    }
+
+    void MoveUp()
+    {
+        if (gameState == state.controlling)
+        {
+            TryFillDirection(Vector3Int.up);
+        }
+    }
+
+    void TryFillDirection(Vector3Int direction)
+    {
+        TileBase nextTile = tilemap.GetTile(currentPosition + direction);
+        if (obstacles.Contains(nextTile)) return;
+        FillLine(direction);
+    }
+
+    void FillLine(Vector3Int direction)
+    {
+        gameState = state.animating;
+        for (int i = 0; i < 20; i++)
+        {
+            Debug.Log(currentPosition);
+            TileBase nextTile = tilemap.GetTile(currentPosition + direction);
+            if (obstacles.Contains(nextTile))
+            {
+                break;
+            }
+            else
+            {
+                currentPosition += direction;
+                tilemap.SetTile(currentPosition, rootTile);
+            }
+        }
+        gameState = state.controlling;
     }
 }
